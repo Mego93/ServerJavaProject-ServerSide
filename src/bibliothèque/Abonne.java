d8a5316c295ac65852af32e6969ee8fa6fed0770
Mego93 <thierryvo2@yahoo.fr>
2019-12-19 00:27:34 +0100

    modif abonne
    arraylist doc enlevé

--- src/bibliothèque/Abonne.java
+++ src/bibliothèque/Abonne.java
@@ -1,16 +1,12 @@
 package bibliothèque;
 
-import java.util.ArrayList;
-
 public class Abonne {
 	private int numero;
 	private String nom;
-	private ArrayList<Document> listeDocs; // Inutile ?
 	
 	public Abonne(int numero, String nom) {
 		this.numero = numero;
 		this.nom = nom;
-		this.listeDocs = new ArrayList<>();
 	}
 
 
@@ -22,9 +18,6 @@ public class Abonne {
 		return nom;
 	}
 
-	public ArrayList<Document> getListeDocs() {
-		return listeDocs;
-	}
 
 
 	
